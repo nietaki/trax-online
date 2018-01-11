ad42a9ce7489d8201fcae7bd0b49becbfcd2fac6
Jacek Królikowski <nietaki@gmail.com>
2018-01-11 21:38:34 +0000

    Move from Elm hello world to a program

--- apps/trax_web/assets/elm/src/Main.elm
+++ apps/trax_web/assets/elm/src/Main.elm
@@ -3,6 +3,41 @@ module Main exposing (..)
 import Html
 
 
-main : Html.Html a
+main : Program Never Model Msg
 main =
-    Html.text "Hello from Elm"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+type Msg
+    = Foo
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Foo ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.text "hello from normal program"
