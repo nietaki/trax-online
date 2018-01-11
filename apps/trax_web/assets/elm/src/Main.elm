bd7a021144828731ad7ba0278bae713672b0995e
Jacek Królikowski <nietaki@gmail.com>
2018-01-11 23:22:12 +0000

    I know kung-fu

--- apps/trax_web/assets/elm/src/Main.elm
+++ apps/trax_web/assets/elm/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Array exposing (Array)
 import Html
+import Html.Events exposing (onClick)
 
 
 -- CONSTANTS
@@ -32,8 +33,12 @@ type FieldState
     | Off
 
 
+type alias RowState =
+    Array FieldState
+
+
 type alias BoardState =
-    Array (Array FieldState)
+    Array RowState
 
 
 type alias Model =
@@ -53,14 +58,38 @@ init =
 
 
 type Msg
-    = Foo
+    = Flip Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Foo ->
-            ( model, Cmd.none )
+    let
+        whatever =
+            Debug.log "incoming message" msg
+    in
+        case msg of
+            Flip row col ->
+                ( flipModel row col model, Cmd.none )
+
+
+flipModel : Int -> Int -> BoardState -> BoardState
+flipModel row col model =
+    arrayUpdate row (flipColumn col) model
+
+
+flipColumn : Int -> RowState -> RowState
+flipColumn col rowState =
+    arrayUpdate col flipField rowState
+
+
+flipField : FieldState -> FieldState
+flipField fieldState =
+    case fieldState of
+        Off ->
+            On
+
+        On ->
+            Off
 
 
 subscriptions : Model -> Sub Msg
@@ -73,17 +102,39 @@ view model =
     boardView model
 
 
+fieldView : Int -> Int -> FieldState -> Html.Html Msg
 fieldView row col fieldState =
-    Html.td [] [ Html.text (fieldText row col fieldState) ]
+    Html.td [ onClick <| Flip row col ] [ Html.text (fieldText row col fieldState) ]
 
 
+rowView : Int -> RowState -> Html.Html Msg
 rowView col rowState =
     Html.tr [] (Array.toList (Array.indexedMap (fieldView col) rowState))
 
 
+boardView : BoardState -> Html.Html Msg
 boardView boardState =
     Html.table [] (Array.toList (Array.indexedMap rowView boardState))
 
 
+fieldText : a -> b -> c -> String
 fieldText row col fieldState =
     (toString row) ++ " " ++ (toString col) ++ " " ++ (toString fieldState)
+
+
+
+-- HELPER FUNCTIONS
+
+
+arrayUpdate : Int -> (a -> a) -> Array a -> Array a
+arrayUpdate index fun array =
+    let
+        originalElement =
+            Array.get index array
+    in
+        case originalElement of
+            Just a ->
+                Array.set index (fun a) array
+
+            Nothing ->
+                array
