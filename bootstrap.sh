e8b0e425d7a64c7eda4b9f6d7fa74551eac152ef
Jacek Królikowski <nietaki@gmail.com>
2018-02-22 21:53:27 +0000

    bootstrapping the project on linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,9 @@ popd
 rm -rf $ROOT_DIR/apps/trax_web/assets/elm/elm-stuff/
 
 pushd $ROOT_DIR/apps/trax_web/assets/
+
+# can't install it on linux otherwise
+sudo npm install --unsafe-perm=true --allow-root -g elm
 npm install
 # this might spit out all sorts of errors the first time it's run
 # the second time around it should be OK
